5a8e1e2c049c712ed47ea63ecf02ced08eccab1a
obsproth <obsproth@users.noreply.github.com>
2017-01-29 16:05:37 +0900

    implemented loading csv from device/cloud storage using Storage Access Framework

--- app/src/main/java/com/github/obsproth/obspasswordforandroid/MainActivity.java
+++ app/src/main/java/com/github/obsproth/obspasswordforandroid/MainActivity.java
@@ -1,13 +1,65 @@
 package com.github.obsproth.obspasswordforandroid;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.github.obsproth.obspassword.ServiceElement;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int READ_REQUEST_CODE = 1;
+    
+    private MyAdapter myAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        myAdapter = new MyAdapter(this);
+        recyclerView.setAdapter(myAdapter);
+        
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("text/*");
+        startActivityForResult(intent, READ_REQUEST_CODE);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        if (requestCode == READ_REQUEST_CODE && resultCode == RESULT_OK) {
+            Uri uri = resultData.getData();
+            List<ServiceElement> elementList = loadCSV(uri);
+            myAdapter.addAll(elementList);
+        }
+    }
+
+    private List<ServiceElement> loadCSV(Uri uri) {
+        try {
+            InputStream inputStream = getContentResolver().openInputStream(uri);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            List<ServiceElement> list = new ArrayList<>();
+            while ((line = reader.readLine()) != null) {
+                list.add(ServiceElement.buildFromCSV(line));
+            }
+            return list;
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
